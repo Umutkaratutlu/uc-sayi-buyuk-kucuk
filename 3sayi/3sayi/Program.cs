f74a1acd051262da0399a8427dcb8a65f63b769d
Umutkaratutlu <107753189+Umutkaratutlu@users.noreply.github.com>
2025-02-20 23:26:37 +0300

    uc-sayi

---
+++ 3sayi/3sayi/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+   
+    Console.Write("Birinci sayıyı girin: ");
+    double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("İkinci sayıyı girin: ");
+    double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Üçüncü sayıyı girin: ");
+    double sayi3 = Convert.ToDouble(Console.ReadLine());
+
+    double enBuyuk;
+
+    if (sayi1 >= sayi2 && sayi1 >= sayi3)
+    {
+        enBuyuk = sayi1;
+    }
+    else if (sayi2 >= sayi1 && sayi2 >= sayi3)
+    {
+        enBuyuk = sayi2;
+    }
+    else
+    {
+        enBuyuk = sayi3;
+    }
+
+   
+    Console.WriteLine("En büyük sayı: " + enBuyuk);
+}
